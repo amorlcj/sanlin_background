ce46622896143b60ea7549d53e653b14a9595899
amorlcj <871283069@qq.com>
2018-12-07 15:45:45 +0800

    first commit

---
+++ src/main/java/com/littleprogram/sanlin_background/SanlinBackgroundApplication.java
@@ -0,0 +1,12 @@
+package com.littleprogram.sanlin_background;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanlinBackgroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanlinBackgroundApplication.class, args);
+    }
+}
---
+++ src/main/java/com/littleprogram/sanlin_background/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.littleprogram.sanlin_background.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+    @RequestMapping("/hello")
+    private String index(){
+        Jedis jedis=new Jedis("127.0.0.1",6379);
+        jedis.set("hello","lcj");
+        String ss=jedis.get("hello");
+        return ss;
+    }
+}
